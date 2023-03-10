7319986c84d1b865313b4c50af97e64b074e3e5f
mateusmaciel340 <mateusmaciel340@gmail.com>
2023-03-10 16:17:57 -0300

    feat: operadores de atribuição em javascript

---
+++ javascript-basico/05-operadores-atribuicao.js
@@ -0,0 +1,8 @@
+let valorPrincipal = 1;
+
+console.log(valorPrincipal + " + 20 = " + (valorPrincipal += 20));
+console.log(valorPrincipal + " - 20 = " + (valorPrincipal -= 20));
+console.log(valorPrincipal + " * 20 = " + (valorPrincipal *= 20));
+console.log(valorPrincipal + " / 20 = " + (valorPrincipal /= 20));
+console.log(valorPrincipal + " % 20 = " + (valorPrincipal %= 20));
+console.log(valorPrincipal + " ** 20 = " + (valorPrincipal **= 20));
