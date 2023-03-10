b79bcfbc9ce61546acd69d7c1ba04a7000c77310
mateusmaciel340 <mateusmaciel340@gmail.com>
2023-03-10 16:17:41 -0300

    feat: operadores aritméticos em javascript

---
+++ javascript-basico/04-operadores-aritmeticos.js
@@ -0,0 +1,9 @@
+let primeiroValor = 2;
+let segundoValor = 2;
+
+console.log(primeiroValor + " + " + segundoValor + " = " + (primeiroValor + segundoValor));
+console.log(primeiroValor + " - " + segundoValor + " = " + (primeiroValor - segundoValor));
+console.log(primeiroValor + " * " + segundoValor + " = " + (primeiroValor * segundoValor));
+console.log(primeiroValor + " / " + segundoValor + " = " + (primeiroValor / segundoValor));
+console.log(primeiroValor + " % " + segundoValor + " = " + (primeiroValor % segundoValor));
+console.log(primeiroValor + " ** " + segundoValor + " = " + (primeiroValor ** segundoValor));
