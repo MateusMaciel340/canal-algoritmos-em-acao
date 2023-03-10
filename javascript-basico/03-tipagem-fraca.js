0081740cde831524009013d293e70c7bd5f6307b
mateusmaciel340 <mateusmaciel340@gmail.com>
2023-03-10 16:17:27 -0300

    feat: tipagem fraca em javascript

---
+++ javascript-basico/03-tipagem-fraca.js
@@ -0,0 +1,17 @@
+let qualquerCoisa = "Olá Mundo!";
+console.log(qualquerCoisa + " -> " + typeof qualquerCoisa);
+
+qualquerCoisa = 123;
+console.log(qualquerCoisa + " -> " + typeof qualquerCoisa);
+
+qualquerCoisa = 23.5;
+console.log(qualquerCoisa + " -> " + typeof qualquerCoisa);
+
+qualquerCoisa = false;
+console.log(qualquerCoisa + " -> " + typeof qualquerCoisa);
+
+qualquerCoisa = null;
+console.log(qualquerCoisa + " -> " + typeof qualquerCoisa);
+
+qualquerCoisa = undefined;
+console.log(qualquerCoisa + " -> " + typeof qualquerCoisa);
