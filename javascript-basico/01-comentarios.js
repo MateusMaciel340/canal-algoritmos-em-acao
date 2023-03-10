22f1fe16a41cd559f2f2d45f3eec88464707f490
mateusmaciel340 <mateusmaciel340@gmail.com>
2023-03-10 16:17:05 -0300

    feat: comentários em javascript

---
+++ javascript-basico/01-comentarios.js
@@ -0,0 +1,15 @@
+console.log("Hello World"); // Imprimir uma mensagem na tela
+
+// Comentário de uma linha
+console.log("Comentário de uma linha");
+
+/* Comentário de uma linha (abertura e fechamento) */
+console.log("Comentário de uma linha (abertura e fechamento)");
+
+/*
+ * Comentário de diversas linhas (abertura e fechamento)
+ */
+
+console.log("Comentário de diversas linhas (abertura e fechamento)");
+
+/* ------------------------------------------------------------------------ */
